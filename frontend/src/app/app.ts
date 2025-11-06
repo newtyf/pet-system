11a13539e76c07004a05b1d916c5442e015ef83d
newtyf <axelskam.123@outlook.es>
2025-11-06 15:16:22 -0500

    feat(frontend): :sparkles: update Angular dependencies and implement Material Design components

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
